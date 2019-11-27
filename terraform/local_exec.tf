81f34c8b56aa5c7846c2a18c1e85883c34acad87
srikanthambati999 <49298862+srikanthambati999@users.noreply.github.com>
2019-11-27 14:27:37 +0530

    Update local_exec.tf

--- terraform/local_exec.tf
+++ terraform/local_exec.tf
@@ -7,13 +7,13 @@ terraform {
 
 provider "aws" {
   version = "~> 2.0"
-  region     = "us-east-1"
+  region     = "us-east-2"
   access_key = "AKIAZDHBYI2Y4LK37YA2"
   secret_key = "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"
   }
 
 resource "aws_instance" "backend" {
-  ami                    = "ami-04763b3055de4860b"
+  ami                    = "ami-0891395d749676c2e"
   instance_type          = "t2.micro"
   key_name               = "${var.key_name}"
   vpc_security_group_ids = ["${var.sg-id}"]
